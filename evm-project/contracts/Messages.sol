bc415048d25df97d857fb51356d8bd61a417f9c3
keijak97 <94452772+keijak97@users.noreply.github.com>
2022-11-10 23:26:20 +0545

    Removed Unused Struct

--- evm-project/contracts/Messages.sol
+++ evm-project/contracts/Messages.sol
@@ -16,14 +16,6 @@ contract Messages {
         bytes account;
         bytes tokenMint;
     }
-    struct UpdateStream {
-        uint64 start_time;
-        uint64 end_time;
-        uint64 amount;
-        uint256 toChain;
-        bytes sender;
-        bytes receiver;
-    }
 
     struct UpdateStreamToken {
         uint64 start_time;
@@ -34,17 +26,6 @@ contract Messages {
         bytes receiver;
         bytes token_mint;
         bytes data_account_address;
-    }
-
-    struct ProcessStream {
-        uint64 start_time;
-        uint64 end_time;
-        uint64 amount;
-        uint256 toChain;
-        bytes sender;
-        bytes receiver;
-        uint64 can_cancel;
-        uint64 can_update;
     }
 
     struct ProcessStreamToken {
@@ -59,22 +40,12 @@ contract Messages {
         bytes token_mint;
     }
 
-    struct ProcessWithdrawStream {
-        uint256 toChain;
-        bytes withdrawer;
-    }
-
     struct ProcessWithdrawStreamToken {
         uint256 toChain;
         bytes withdrawer;
         bytes token_mint;
         bytes sender_address;
         bytes data_account_address;
-    }
-    
-    struct PauseStream {
-        uint256 toChain;
-        bytes sender;
     }
 
     struct PauseStreamToken {
@@ -85,11 +56,6 @@ contract Messages {
         bytes data_account_address;
     }
 
-    struct CancelStream {
-        uint256 toChain;
-        bytes sender;
-    }
-
     struct CancelStreamToken {
         uint256 toChain;
         bytes sender;
@@ -98,30 +64,11 @@ contract Messages {
         bytes data_account_address;
     }
 
-    struct ProcessDeposit {
-        uint64 amount;
-        uint256 toChain;
-        bytes depositor;
-    }
-
     struct ProcessDepositToken {
         uint64 amount;
         uint256 toChain;
         bytes depositor;
         bytes token_mint;
-    }
-
-    struct ProcessWithdraw {
-        uint64 amount;
-        uint256 toChain;
-        bytes withdrawer;
-    }
-
-    struct ProcessTransfer {
-        uint64 amount;
-        uint256 toChain;
-        bytes withdrawer;
-        bytes sender;
     }
 
     struct ProcessTransferToken {
@@ -139,9 +86,4 @@ contract Messages {
         bytes token_mint;
     }
 
-    struct InstantWithdrawal {
-        uint64 amount;
-        uint256 toChain;
-        bytes withdrawer;
-    }
 }
