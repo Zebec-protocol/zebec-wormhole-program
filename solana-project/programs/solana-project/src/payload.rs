f9f3d66a4c822fe62382f6816fc14cb8a904ff8f
roshans <shrestharoshan768@gmail.com>
2022-12-16 08:21:48 +0545

    feat: Add payload structs to decode payload bytes

---
+++ solana-project/programs/solana-project/src/payload.rs
@@ -0,0 +1,89 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct XstreamStartPayload {
+    pub start_time: u64,
+    pub end_time: u64,
+    pub amount: u64,
+    pub to_chain_id: u64,
+    pub sender: [u8; 32],
+    pub receiver: [u8; 32],
+    pub can_update: u64,
+    pub can_cancel: u64,
+    pub token_mint: [u8; 32],
+}
+
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct XstreamDepositPayload {
+    pub amount: u64,
+    pub to_chain_id: u64,
+    pub sender: [u8; 32],
+    pub token_mint: [u8; 32],
+}
+
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct XstreamUpdatePayload {
+    pub start_time: u64,
+    pub end_tme: u64,
+    pub amount: u64,
+    pub to_chain_id: u64,
+    pub sender: [u8; 32],
+    pub receiver: [u8; 32],
+    pub can_update: u64,
+    pub can_cancel: u64,
+    pub token_mint: [u8; 32],
+    pub data_account: [u8; 32],
+}
+
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct XstreamPausePayload {
+    pub to_chain_id: [u8; 32],
+    pub depositor: [u8; 32],
+    pub token_mint: [u8; 32],
+    pub receiver: [u8; 32],
+    pub data_account: [u8; 32],
+}
+
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct XstreamWithdrawPayload {
+    pub to_chain_id: [u8; 32],
+    pub withdrawer: [u8; 32],
+    pub token_mint: [u8; 32],
+    pub depositor: [u8; 32],
+    pub data_account: [u8; 32],
+}
+
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct XstreamCancelPayload {
+    pub to_chain_id: [u8; 32],
+    pub depositor: [u8; 32],
+    pub token_mint: [u8; 32],
+    pub receiver: [u8; 32],
+    pub data_account: [u8; 32],
+}
+
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct XstreamWithdrawDepositPayload {
+    pub amount: u64,
+    pub to_chain_id: [u8; 32],
+    pub withdrawer: [u8; 32],
+    pub token_mint: [u8; 32],
+}
+
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct XstreamInstantTransferPayload {
+    pub amount: u64,
+    pub to_chain_id: [u8; 32],
+    pub sender: [u8; 32],
+    pub token_mint: [u8; 32],
+    pub withdrawer: [u8; 32],
+}
+
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct XstreamDirectTransferPayload {
+    pub amount: u64,
+    pub to_chain_id: [u8; 32],
+    pub sender: [u8; 32],
+    pub token_mint: [u8; 32],
+    pub withdrawer: [u8; 32],
+}
